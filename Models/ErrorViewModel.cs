2f8c3ef71237001a5252d6e4caea8f4d37298fb4
Gustavo D. Silva <104632552+CAKeysama@users.noreply.github.com>
2025-08-28 12:43:40 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HortalisCSharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
