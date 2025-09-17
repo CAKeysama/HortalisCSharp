ea059054cd079bb6957339ec629c1954900b46fe
Gustavo D. Silva <104632552+CAKeysama@users.noreply.github.com>
2025-09-17 08:59:01 -0300

    Fix: Paginas Autores e Tutoriais
    -Falta criar conteudos dos Tabs na pagina Tutoriais.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace HortalisCSharp.Controllers
             return View();
         }
 
+        public IActionResult Autores()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
